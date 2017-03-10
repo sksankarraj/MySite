5076e4dc4d7926e663e456f3358f748a728873ac
sksankarraj <sksankarraj@gmail.com>
2017-03-10 14:46:23 +0530

    UI changed

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 var MySite = angular.module('mine',['ui.router']);
 
-MySite.config(function($stateProvider, $urlRouterProvider){
+MySite.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
     $urlRouterProvider.otherwise('/');
     $stateProvider
         .state('/', {
             url:'/',
             templateUrl:'web/front.html'
         });
-});
+        $locationProvider.html5Mode(true);
+}]);
 
 MySite.run(function(){
     console.log("I'm OK");
