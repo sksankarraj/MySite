d786ce47d9a84b2e494f7fac4108283e11b2e482
sksankarraj <sksankarraj@gmail.com>
2017-03-16 22:47:53 +0530

    Added Thirukural

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
-var MySite = angular.module('mine',['ui.router']);
+var MySite = angular.module('mine',['ui.router','facebook']);
 
 MySite.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
     $urlRouterProvider.otherwise('/');
     $stateProvider
         .state('/', {
             url:'/',
-            templateUrl:'web/front.html'
+            templateUrl:'web/front.html',
+            controller:'indexController'
         });
         $locationProvider.html5Mode(true);
 }]);
-
+MySite.config(function(FacebookProvider) {
+        FacebookProvider.init('1292236160864774');
+});
 MySite.run(function(){
     console.log("I'm OK");
+});
+MySite.controller('quoteController',function($scope, $http){
+    var i = Math.floor(Math.random() * 1300);
+    $http.get('./thirukural.json')
+        .success(function(data){
+            $scope.qoute=data.kurals[i];
+        })
+        .error(function(resp){
+            console.log(resp);
+        });
+});
+MySite.controller('indexController',function($scope,Facebook,$timeout){
+
 });
--- js/mis.js
+++ js/mis.js
@@ -1,5 +1,52 @@
 $(document).ready(function(){
-    $('center').remove();
     $('div[style]').remove();
-    $('script[language="JavaScript"]').remove();
+    var qTime = 10000, i = 0;
+    $('#loadTime').text(qTime/1000);
+    $('#btnP').addClass('fapause');
+    var loadTOut= function(Tcb,Icb,time){
+        var Tid = setTimeout(Tcb,time);
+        var Iid = setInterval(Icb,1000);
+        this.isActive=true;
+        this.stop = function(){
+            this.isActive = false;
+            clearTimeout(Tid);
+            clearInterval(Iid);
+        };
+    };
+    var int = function(){
+       i += 1;
+        $('#loadTime').text($('#loadTime').text() - 1);
+         $('.loader').animate({
+            "width": (i/((qTime/1000)-1)) * 100 + '%'
+        },1000);
+    };
+    var tOut = function(){
+            $('.qoute').animate( {
+                "left":"-100%"
+            },300);
+            t.stop();
+        };
+    var t = new loadTOut(tOut,int,qTime);
+    $('#btnP').click(function(){
+        if (t.isActive){
+            t.stop();
+            $(this).removeClass('fapause');
+            $(this).addClass('faplay');
+        }
+        else{
+            t = new loadTOut(tOut,int,$('#loadTime').text()*1000);
+            $(this).removeClass('faplay');
+            $(this).addClass('fapause');
+        }
+    });
+    
+    // var tOut = setTimeout(function(){
+    //         $('.qoute').animate( {
+    //             "left":"-100%"
+    //         },300);
+    //         clearInterval(int);
+    //     },qTime);
+    // $('img').hover(function(){
+
+    // });
 });
